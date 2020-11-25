a4c20ab750820c03b28bfc2332a3be02102bbbec
dohyun0324 <dohyun0324>
2020-11-26 01:12:09 +0900

    fix a little

--- moco.py
+++ moco.py
@@ -12,34 +12,7 @@ import os
 import time
 import math
 from torchvision.models.resnet import conv3x3
-
-class BasicBlock(nn.Module):
-    def __init__(self, inplanes, planes, norm_layer, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.downsample = downsample
-        self.stride = stride
-        
-        self.bn1 = norm_layer(inplanes)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        
-        self.bn2 = norm_layer(planes)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-
-    def forward(self, x):
-        residual = x 
-        residual = self.bn1(residual)
-        residual = self.relu1(residual)
-        residual = self.conv1(residual)
-
-        residual = self.bn2(residual)
-        residual = self.relu2(residual)
-        residual = self.conv2(residual)
-
-        if self.downsample is not None:
-            x = self.downsample(x)
-        return x + residual
+import torchvision.models as models
 
 class Downsample(nn.Module):
     def __init__(self, nIn, nOut, stride):
@@ -52,50 +25,6 @@ class Downsample(nn.Module):
         x = self.avg(x)
         return torch.cat([x] + [x.mul(0)] * (self.expand_ratio - 1), 1)
 
-class ResNetCifar(nn.Module):
-    def __init__(self, depth, width=1, classes=10, channels=3, norm_layer=nn.BatchNorm2d):
-        assert (depth - 2) % 6 == 0         # depth is 6N+2
-        self.N = (depth - 2) // 6
-        super(ResNetCifar, self).__init__()
-
-        # Following the Wide ResNet convention, we fix the very first convolution
-        self.conv1 = nn.Conv2d(channels, 16, kernel_size=3, stride=1, padding=1, bias=False)
-        self.inplanes = 16
-        self.layer1 = self._make_layer(norm_layer, 16 * width)
-        self.layer2 = self._make_layer(norm_layer, 32 * width, stride=2)
-        self.layer3 = self._make_layer(norm_layer, 64 * width, stride=2)
-        self.bn = norm_layer(64 * width)
-        self.relu = nn.ReLU(inplace=True)
-        self.avgpool = nn.AvgPool2d(8)
-
-        # Initialization
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                
-    def _make_layer(self, norm_layer, planes, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes:
-            downsample = Downsample(self.inplanes, planes, stride)
-        layers = [BasicBlock(self.inplanes, planes, norm_layer, stride, downsample)]
-        self.inplanes = planes
-        for i in range(self.N - 1):
-            layers.append(BasicBlock(self.inplanes, planes, norm_layer))
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        return x
-
-    
 class Normalize(nn.Module):
 
     def __init__(self, power=2):
@@ -107,69 +36,6 @@ class Normalize(nn.Module):
         out = x.div(norm)
         return out
     
-
-class SimCLRHead(nn.Module):
-    def __init__(self, width, emb_dim):
-        super(SimCLRHead, self).__init__()
-        
-        ### IMPLEMENTATION 1-1 ###
-        ### 1. Linear layer (64 * width -> 64 * width)
-        ### 2. ReLU
-        ### 3. Linear layer (64 * width -> emb_dim)
-        ### 4. Normalization layer (Normalize module above)
-        self.fc1 = nn.Linear(64*width,64*width)
-        self.relu = nn.ReLU(inplace=True)
-        self.fc2 = nn.Linear(64*width,emb_dim)
-        self.norm = Normalize()
-        ### IMPLEMENTATION ENDS HERE ###
-        
-    def forward(self, x):
-        
-        ### IMPLEMENTATION 1-1 ###
-        ### Design a proper forward function
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.norm(x)
-        ### IMPLEMENTATION ENDS HERE ###
-        return x
-    
-
-class SimCLRNet(nn.Module):
-    def __init__(self, depth, width=1, num_classes=10, emb_dim=32):
-        super(SimCLRNet, self).__init__()
-        
-        self.num_classes = num_classes
-        
-        self.feat = ResNetCifar(depth=depth, width=width, classes=num_classes)
-        
-        ### IMPLEMENTATION 1-2 ###
-        ### 1. A projection head (SimCLRHead module above)
-        self.head = SimCLRHead(width,emb_dim)
-        
-        ### 2. A linear classifier (64 * width -> num_classes)
-        self.classifier = nn.Linear(64*width,num_classes)
-        
-        ### 3. Normalization layer for conv feature normalization (Normalize module above)
-        self.norm = Normalize()
-        
-        ### IMPLEMENTATION ENDS HERE ###
-    
-    def forward(self, x, norm_feat=False):
-        
-        ### IMPLEMENTATION 1-2 ###
-        ### Your module must return
-        ### 1. Conv feature (feat) - when norm_feat is true, apply L2 normalization
-        ### 2. Projected embedding (emb)
-        ### 3. Logit vector by the linear classifier (logit)
-        feat = self.feat(x)
-        if norm_feat==True:
-          feat=self.norm(feat)
-        emb = self.head(feat)
-        logit = self.classifier(feat)
-        ### IMPLEMENTATION ENDS HERE ###
-        return feat, emb, logit
-
 class DuplicatedCompose(object):
     def __init__(self, transforms):
         self.transforms = transforms
@@ -215,19 +81,13 @@ img_size = (32, 32)
 color_jitter = transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
 
 train_transform = DuplicatedCompose([
-    ### IMPLEMENTATION 2-1 ###
-    ### 1. Random resized crop w/ final size of (32, 32)
-    ### 2. Random horizontal flip w/ p=0.5
-    ### 3. Randomly apply the pre-defined color jittering w/ p=0.8
-    ### 4. Random gray scale w/ p=0.2
-    ### 5. Gaussian blur w/ kernel size of 1/10 of the image width or height (32)
-    transforms.RandomResizedCrop(size=(32,32)),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomApply([color_jitter],p=0.8),
+    transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
     transforms.RandomGrayscale(p=0.2),
-    GaussianBlur(kernel_size=int(0.1*32)),
-    ### IMPLEMENTATION ENDS HERE ###
+    transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
 ])
 
 from torch.utils.data import DataLoader
@@ -295,16 +155,22 @@ class NTXentLoss(torch.nn.Module):
         # filter out the scores from the positive samples
         l_pos = torch.diag(similarity_matrix, self.batch_size)
         r_pos = torch.diag(similarity_matrix, -self.batch_size)
+     #   print(l_pos)
+       # print(r_pos)
         positives = torch.cat([l_pos, r_pos]).view(2 * self.batch_size, 1)
 
         negatives = similarity_matrix[self.mask_samples_from_same_repr].view(2 * self.batch_size, -1)
 
         logits = torch.cat((positives, negatives), dim=1)
         logits = logits / self.temperature
+       # print(positives.shape)
+     #   print(negatives.shape)
+       # print(logits.shape)
 
         labels = torch.zeros(2 * self.batch_size).cuda().long()
+#        print(labels)
         loss = self.criterion(logits, labels)
-
+     #   print(loss)
         return loss / (2 * self.batch_size)
 
 from torch.optim.optimizer import Optimizer, required
@@ -377,7 +243,7 @@ class SGD_with_lars(Optimizer):
 
 def train(net, loader):
     
-    loss_fn = NTXentLoss(batch_size=256, temperature=0.05, use_cosine_similarity=True)
+    loss_fn = NTXentLoss(encoder = models.resnet50, batch_size=256, temperature=0.05, use_cosine_similarity=True)
     
     ### IMPLEMENTATION 4-2 ###
     ### 1. Use SGD_with_lars with
@@ -409,10 +275,6 @@ def train(net, loader):
             
             dat1 = data[0].cuda()
             dat2 = data[1].cuda()
-            #print(dat1.shape)
-            dat1 = net(dat1)[1]
-            dat2 = net(dat2)[1]
-           # print(dat1.dtype)
             loss = loss_fn(dat1, dat2)
             ### IMPLEMENTATION ENDS HERE ###
             
